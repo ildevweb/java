7565705d17c6829526953cff2e4d2a55cd507006
Ilyass Afriad <ilyassafriad@gmail.com>
2025-10-07 03:40:45 +0100

    StringLength

---
+++ StringLength/ExerciseRunner.java
@@ -0,0 +1,8 @@
+package StringLength;
+
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        System.out.println(StringLength.getStringLength("Hello World !"));
+        System.out.println(StringLength.getStringLength(""));
+    }
+}
