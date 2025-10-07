0c8f8c3e9840a5846c6c2b248de2b7445d52f971
Ilyass Afriad <ilyassafriad@gmail.com>
2025-10-07 03:45:41 +0100

    StringContains

---
+++ StringContains/ExerciseRunner.java
@@ -0,0 +1,9 @@
+package StringContains;
+
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        System.out.println(StringContains.isStringContainedIn("Hell", "Highway to Hell"));
+        System.out.println(StringContains.isStringContainedIn("Hell", "Hello World !"));
+        System.out.println(StringContains.isStringContainedIn("Bonjour", "hello World !"));
+    }
+}
