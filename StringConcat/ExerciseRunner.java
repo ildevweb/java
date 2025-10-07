78db7cc7651d9b4549d5d333669b712015f228dc
Ilyass Afriad <ilyassafriad@gmail.com>
2025-10-07 03:42:58 +0100

    StringConcat

---
+++ StringConcat/ExerciseRunner.java
@@ -0,0 +1,8 @@
+package StringConcat;
+
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        System.out.println(StringConcat.concat("Hello ", "étudiant !"));
+        System.out.println(StringConcat.concat("", "Hello World !"));
+    }
+}
