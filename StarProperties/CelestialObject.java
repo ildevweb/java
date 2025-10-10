2c7c09c7318352044666c5f4a353add36b5cc301
Ilyass Afriad <ilyassafriad@gmail.com>
2025-10-10 20:53:46 +0100

    StarProperties

---
+++ StarProperties/CelestialObject.java
@@ -0,0 +1,8 @@
+package StarProperties;
+
+public class CelestialObject {
+    double x = 0.0;
+    double y = 0.0;
+    double z = 0.0;
+    String name = null;
+}
---
+++ StarProperties/ExerciseRunner.java
@@ -0,0 +1,11 @@
+package StarProperties;
+
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        CelestialObject celestialObject = new CelestialObject();
+        System.out.println(celestialObject.x);
+        System.out.println(celestialObject.y);
+        System.out.println(celestialObject.z);
+        System.out.println(celestialObject.name);
+    }
+}
