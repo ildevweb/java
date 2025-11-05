fa3631c662349a529d45f3dad434aa26c23fa2bd
Ilyass Afriad <ilyassafriad@gmail.com>
2025-11-05 03:21:00 +0100

    ListEquals

---
+++ ListEquals/ExerciseRunner.java
@@ -0,0 +1,11 @@
+package ListEquals;
+
+import java.util.List;
+
+
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        System.out.println(ListEquals.areListsEqual(List.of("Alice", "Bob", "Charly", "Emily"), List.of("Alice", "Bob", "Charly", "Emily")));
+        System.out.println(ListEquals.areListsEqual(List.of("Alice", "Bob", "Charly", "Emily"), List.of("Alice", "Bob", "Emily", "Charly")));
+    }
+}
